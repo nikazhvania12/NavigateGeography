c17905572124acd5ee69a38f3c6c8f874c44abcc
Nikoloz Zhvania <nzhvania@credo.ge>
2025-05-11 09:33:54 +0400

    final commit

---
+++ navigate-geography-client/src/Controllers/Users/LogoutUser.jsx
@@ -0,0 +1,23 @@
+import PackageJSON from '../../../package.json'
+
+async function LogoutUser() {
+
+    const url = PackageJSON.API.BaseURL + PackageJSON.API.Logout;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            credentials: 'include',
+        });
+            
+        if (!response.ok) return [];
+
+        return true;
+    }
+    catch(e) {
+        return [];
+    }
+
+}
+
+export default LogoutUser;
